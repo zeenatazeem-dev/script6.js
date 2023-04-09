31f121a6376220d3b6fe65b5b26ef521f45c1995
Zeenat Aazeem <Shadbkhat13@gmail.com>
2023-04-10 00:17:26 +0500

    task

--- prject.js/v.js
+++ prject.js/v.js
@@ -1,1 +1,10 @@
-alert("Zeenat");
+//alert("Zeenat");
+
+
+var Zakat_persantage =0.025; //2.5%
+var user_input  = Number(prompt("Enter your Amount"));
+var result = 0.025 * user_input;
+alert("your zakat is " + result);
+
+
+
